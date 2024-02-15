a53cbe0d0e11f01ca7e066bbaf37e530b801b680
juniorcintra <junior.cintra.developer@gmail.com>
2024-02-15 04:40:25 -0300

    Inserindo primeiros valores no banco de memoria

--- server.js
+++ server.js
@@ -1,9 +1,41 @@
-import { createServer } from "node:http";
+// import { createServer } from "node:http";
 
-const server = createServer((request, response) => {
-  response.write("oi");
+// const server = createServer((request, response) => {
+//   response.write("oi");
 
-  return response.end();
+//   return response.end();
+// });
+
+// server.listen(3333);
+
+import { fastify } from "fastify";
+import { DatabaseMemory } from "./database-memory.js";
+
+const server = fastify();
+
+const database = new DatabaseMemory();
+
+server.post("/videos", (request, reply) => {
+  const { title, description, duration } = request.body;
+  console.log("🚀 ~ body:", body);
+
+  database.create({ title, description, duration });
+
+  return reply.status(201).send();
 });
 
-server.listen(3333);
+server.get("/videos", () => {
+  return "Hello World!";
+});
+
+server.put("/videos/:id", () => {
+  return "Hello World!";
+});
+
+server.delete("/videos/:id", () => {
+  return "Hello World!";
+});
+
+server.listen({
+  port: 3333,
+});
